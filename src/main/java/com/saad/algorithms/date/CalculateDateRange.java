199c1f94cac1ffccbf36ba3e7d1add599fc8042f
saad1504 <saad.syed.adeeb@gmail.com>
2018-03-08 16:45:42 +0530

    last day of todate not crossing current date

--- src/main/java/com/saad/algorithms/date/CalculateDateRange.java
+++ src/main/java/com/saad/algorithms/date/CalculateDateRange.java
@@ -1,15 +1,45 @@
 package com.saad.algorithms.date;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class CalculateDateRange {
 
 	public static DateRange getDateRange(DateRange input) {
+		boolean flag=false;
 		String fDay = input.fromDate.substring(0, 8);
 		String fDate = fDay + "01";
 		String tYear = input.toDate.substring(0, 4);
 		String tMonth = input.toDate.substring(5, 7);
 		String tDay = null;
+		Calendar cal = Calendar.getInstance();
+		Date date = cal.getTime();             
+		SimpleDateFormat reqFormat = new SimpleDateFormat("yyyy-MM-dd");          
+		String curDate = null;
+		curDate = reqFormat.format(date);
+		String cYear= curDate.substring(0, 4);
+		String cMonth= curDate.substring(5, 7);
+		int cyrint=Integer.parseInt(cYear);
+		int cmint=Integer.parseInt(cMonth);
+		
 		int tint = Integer.parseInt(tMonth);
 		int tyrint=Integer.parseInt(tYear);
+		if(cyrint<=tyrint)
+		{
+			if(cyrint==tyrint)
+			{
+				if(cmint<=tint)
+				{
+					flag=true;
+					
+				}
+			}
+			if(cyrint <tyrint)
+			{
+				flag=true;
+			}
+		}
 		switch (tint) {
 		case 1:
 		case 3:
@@ -37,6 +67,13 @@ public class CalculateDateRange {
 		}
 		}
 		String tDate = tYear + "-" + tMonth + "-" + tDay;
-		return new DateRange(fDate, tDate);
+		
+		
+	
+		if(flag)
+			return new DateRange(fDate,curDate);
+		else
+			
+			return new DateRange(fDate, tDate);
 	}
 }
